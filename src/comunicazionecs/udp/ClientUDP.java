d740ea3a39bab81e97cfab2f02dcb623e1cc0153
RamacciCri <cristianramacci56@gmail.com>
2024-04-12 11:54:56 +0200

    Ultimazione classe ServerUDP e creazione classe ClientUDP

--- src/comunicazionecs/udp/ClientUDP.java
+++ src/comunicazionecs/udp/ClientUDP.java
@@ -1,18 +1,13 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package comunicazionecs.udp;
 
 /**
  *
- * @author monni
+ * @author RamacciCri
  */
 public class ClientUDP {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
         // TODO code application logic here
     }
